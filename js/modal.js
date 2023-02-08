a67e003107405922da9860221259ca804bd7aef9
alejandrardecajal <alejandrardecajal@gmail.com>
2023-02-08 20:17:36 -0300

    Checkbox funcional

--- js/modal.js
+++ js/modal.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
     $("#submit").click(function () {
         $(".error").remove();
         if ($(".nombre").val() == "") {
-            $(".nombre").focus().after(" ", "<span class='error'>Ingrese su nombre</p>");
+            $(".nombre").focus().after(" ", "<span class='error'>Ingrese su nombre</span>");
             return false;
 
         } else if ($(".email").val() == "" || !emailreg.test($(".email").val())) {
@@ -33,37 +33,25 @@ $(document).ready(function () {
             $(".dni").focus().after("<span class='error'>Ingrese su DNI</span>");
             return false;
 
-        // } else if ($(".checkbox").val() == "") {
-        //     $(".checkbox").focus().after("<span class='error'>Por favor acepte para continuar</span>");
-        //     return false;
-        // }
-        }
-    });
-
-    //Se elimina el error al colocar correctamente los campos (fadOut)
-    $(".nombre, .email, .dni").keyup(function () {
-        if ($(this).val() != "") {
-            $(".error").fadeOut();
+        } else if ($("#checkbox").prop("checked") == false) {
+            $("#checkbox").focus().after("<span class='error'>Por favor acepte para continuar</span>");
             return false;
         }
-    });
-
-
-    $("#checkbox").change(function () {
-        if (this.checked) {
-            $(".error").fadeOut();
-            return false;
-        }
-    });
-
-    // $( "form" ).submit(function() {
-    //     return this.checked;
-    //   });
-
-
-    
+    })
 });
 
-// $(".checkbox").prop('checked', this.checked);
+//Se elimina el error al colocar correctamente los campos (fadOut)
+$(".nombre, .email, .dni").keyup(function () {
+    if ($(this).val() != "") {
+        $(".error").fadeOut();
+        return false;
+    }
+});
 
-// $('#checkbox').required = true;
+
+$("#checkbox").change(function () {
+    if (this.checked) {
+        $(".error").fadeOut();
+        return false;
+    }
+});
